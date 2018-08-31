49391cd30039ef11985cf9fb59185ca757b4f1dc
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-08-31 09:13:49 +0300

    fix: example for batches

--- example/example.js
+++ example/example.js
@@ -1,5 +1,10 @@
-import { QueryServiceClient, CommandServiceClient } from '../lib/proto/endpoint_pb_service'
+import {
+  QueryServiceClient,
+  CommandServiceClient
+} from '../lib/proto/endpoint_pb_service'
 import { queryHelper, txHelper } from '../lib'
+
+import { TxStatus, TxStatusRequest } from '../lib/proto/endpoint_pb.js'
 
 import flow from 'lodash.flow'
 
@@ -10,40 +15,108 @@ let adminPriv =
 
 // Creating transaction with txHelper
 let transaction = flow(
-  (t) => txHelper.addCommand(t, 'CreateAsset', { assetName: 'dollar', domainId: 'test', precision: 2 }),
-  (t) => txHelper.addMeta(t, { creatorAccountId: 'admin@test', createdTime: Date.now() }),
-  (t) => txHelper.sign(t, adminPriv)
+  t =>
+    txHelper.addCommand(t, 'CreateAsset', {
+      assetName: 'dollar3',
+      domainId: 'test',
+      precision: 2
+    }),
+  t =>
+    txHelper.addMeta(t, {
+      creatorAccountId: 'admin@test',
+      createdTime: Date.now()
+    }),
+  t => txHelper.sign(t, adminPriv)
 )(txHelper.emptyTransaction())
 
-const txClient = new CommandServiceClient(
-  irohaAddress
-)
+let transaction2 = flow(
+  t =>
+    txHelper.addCommand(t, 'CreateAsset', {
+      assetName: 'dollar2',
+      domainId: 'test',
+      precision: 2
+    }),
+  t =>
+    txHelper.addMeta(t, {
+      creatorAccountId: 'admin@test',
+      createdTime: Date.now()
+    })
+)(txHelper.emptyTransaction())
 
-// Submitting transaction
-txClient.torii(transaction, (err, data) => {
+const txClient = new CommandServiceClient(irohaAddress)
+
+const txHash = txHelper.hash(transaction)
+const txHash2 = txHelper.hash(transaction2)
+
+const batch = txHelper.createBatch([transaction, transaction2], 0)
+
+txClient.listTorii(batch, (err, data) => {
   if (err) {
     throw err
   } else {
-    console.log('Submitted transaction successfully! Hash: ' + txHelper.hash(transaction).toString('hex'))
+    console.log(
+      'Submitted transaction successfully! Hash: ' +
+        txHash.toString('hex')
+    )
   }
 })
 
-// Creating query with queryHelper
-let query = flow(
-  (q) => queryHelper.addQuery(q, 'getAccount', { accountId: 'admin@test' }),
-  (q) => queryHelper.addMeta(q, { creatorAccountId: 'admin@test' }),
-  (q) => queryHelper.sign(q, adminPriv)
-)(queryHelper.emptyQuery())
+// Submitting transaction
+// txClient.torii(transaction, (err, data) => {
+//   if (err) {
+//     throw err
+//   } else {
+//     console.log(
+//       'Submitted transaction successfully! Hash: ' +
+//         txHash.toString('hex')
+//     )
+//   }
+// })
 
-const queryClient = new QueryServiceClient(
-  irohaAddress
-)
+// // Creating query with queryHelper
+// let query = flow(
+//   (q) => queryHelper.addQuery(q, 'getAccount', { accountId: 'admin@test' }),
+//   (q) => queryHelper.addMeta(q, { creatorAccountId: 'admin@test' }),
+//   (q) => queryHelper.sign(q, adminPriv)
+// )(queryHelper.emptyQuery())
 
-// Sending query with queryHelper
-queryClient.find(query, (err, response) => {
-  if (err) {
-    throw err
-  } else {
-    console.log(JSON.stringify(response))
-  }
+// const queryClient = new QueryServiceClient(
+//   irohaAddress
+// )
+
+// // Sending query with queryHelper
+// queryClient.find(query, (err, response) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log(JSON.stringify(response));
+//   }
+// });
+
+const request = new TxStatusRequest()
+
+request.setTxHash(txHash)
+
+let stream = txClient.statusStream(request)
+
+stream.on('data', function (response) {
+  console.log(response.getTxStatus())
+})
+
+stream.on('end', function (end) {
+  console.log('finish')
+})
+
+const request2 = new TxStatusRequest()
+
+request2.setTxHash(txHash2)
+
+let stream2 = txClient.statusStream(request2)
+
+stream2.on('data', function (response) {
+  console.log(response.getTxStatus())
+})
+
+stream2.on('end', function (end) {
+  console.log('finish')
 })
